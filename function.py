7cc5eeeb921792ac376eea51d3bb10f99a4485ff
S-Nakase <s.nakase2282002@gmail.com>
2023-02-13 13:33:19 +0900

    second

---
+++ function.py
@@ -0,0 +1,11 @@
+import sys, os
+import gc
+import time
+import pandas as pd
+import numpy as np
+import codecs
+from glob import glob
+from pathlib import Path
+from googletrans import Translator
+
+
